7c190f614bc00b70a0a84481d718ab4e842ca741
SchmuhlFace <eschmuhl@gmail.com>
2016-09-01 13:12:04 -0400

    Create a method for fibonacci numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,5 @@
+
+#Release 0
 #create the method that intakes an array and a number
 #evaluate each component of the array and see if it is equal to num
 #if it is, return the index value
@@ -17,4 +19,25 @@ end
  arr = [ 2, 4, 6, 8]
 
  search_array(arr, 2)
+
+#Release 1
+
+def fibonacci(num)
+  arr = [0] 
+  num.times do |x|
+    if x == 0 
+      arr[x] = 0
+    elsif x == 1
+      arr[x] = 1
+    else 
+      arr[x] = arr[x-1] + arr[x-2]
+    end
+  end
+
+return arr[0...num]
+
+end
+
+puts fibonacci(100)
+
 
