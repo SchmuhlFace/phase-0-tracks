8d9e182972b8f601ec952a901dff5341ae8606b1
SchmuhlFace <eschmuhl@gmail.com>
2016-09-06 14:16:53 -0400

    Add all release work and a helpful note at bottom of doc

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,13 +1,23 @@
-SPECIES ------------------------
-Puppy!
+##RELEASE 0
 
-CHARACTERISTICS ----------------
-Friendly: yes
-Name: varies
-Color: varies
+# SPECIES ------------------------
+# Puppy! 
 
-BEHAVIOR -----------------------
-Walk
-Sleep
-Swim
-Eat
+# CHARACTERISTICS ----------------
+# Friendly: yes
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Walk
+# Sleep
+# Swim
+# Sit
+# Shake
+
+
+
+##RELEASE 1 & 2 in IRB
+
+# important to note: this is for the class, Puppy. Instances are occurrences of the class. 
+#An example would be species of Puppy (drawing from spider analogy)
