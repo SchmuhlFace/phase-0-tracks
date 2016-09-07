1fff54e7674219b0680c2c9bdb81de225b9d781a
SchmuhlFace <eschmuhl@gmail.com>
2016-09-07 13:30:28 -0400

    Add psuedocode

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,33 @@
+
+
+##PSEUDOCODE
+
+# Method to create a list: Create an empty hash
+# input: string of items separated by spaces (example: "carrots 2 apples 3 cereal pizza")
+# steps: 
+  # prompt user for a list of items. 
+  # transform user input to hash.
+  # print hash out to the user.  
+  # output: return hash (information and print back the list) and thank the user for the info.
+
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: receive input and push/add new item into hash. 
+# output: updated hash.
+
+# Method to remove an item from the list
+# input: item name and optional quantity 
+# steps: receive input and delete item into hash. 
+# output: updated hash
+
+# Method to update the quantity of an item
+# input: item name and quantity 
+# steps: find item and update the quantity
+# output: updated hash
+
+# Method to print a list and make it look pretty
+# input: Hash unless the hash is something in the driver code (accessible everywhere
+# steps: Format information
+# output: print info
+
