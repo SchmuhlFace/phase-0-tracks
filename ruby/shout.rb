ffc1146c8247e9dfcd1b13169bdd3891f3c802e0
SchmuhlFace <eschmuhl@gmail.com>
2016-09-08 11:50:15 -0400

    Test code and add driver code to bottom with test print outs

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -9,5 +9,12 @@ module Shout
   def self.yelling_happily(words)
     words + "!!!" + " :)"
   end
-  
+
 end
+
+#Driver Code
+
+# p Shout.yell_angrily("hey")
+# => "hey!!! :("
+# p Shout.yelling_happily("hey")
+# => "hey!!! :)"
