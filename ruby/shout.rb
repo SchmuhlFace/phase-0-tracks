9b978c43dc9012dfb795e85ed6f33bfa57de2abc
SchmuhlFace <eschmuhl@gmail.com>
2016-09-08 12:12:40 -0400

    Add mixin version of shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,20 +1,55 @@
 ##Writing my first module!
 
+##Writing my first module!
+
 module Shout
 
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yell_happily(words)
     words + "!!!" + " :)"
   end
 
 end
 
-#Driver Code
+
+# Declare Classes
+
+
+class Human
+  include Shout
+end
+
+class Fox
+  include Shout
+end
+
+
+#Test mixin
+
+# human = Human.new
+#   human.yell_angrily("A bear")
+#   => "A bear!!! :("
+  
+  #human.yell_happily("A bear")
+  #=> "A bear!!! :)"
+
+
+# fox = Fox.new
+
+  #fox.yell_angrily("A human")
+  #=> "A human!!! :("
+  
+  #fox.yell_happily("A human")
+  #=> "A human!!! :)"
+
+## Driver Code - test inital
 
 # p Shout.yell_angrily("hey")
 # => "hey!!! :("
+
+
 # p Shout.yelling_happily("hey")
 # => "hey!!! :)"
