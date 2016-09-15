0e0c89b9779368ca40d07ac220a9e7742ee76184
SchmuhlFace <eschmuhl@gmail.com>
2016-09-15 19:24:58 -0400

    Create two arrays and push two new variables into them

---
+++ js/data_structures.js
@@ -0,0 +1,12 @@
+// array of colors
+var colors = ["purple", "blue", "green", "white"];
+
+//array of four name
+
+var names = ["Snowball", "Unicorn", "Kitten", "Brownie"];
+
+// add another color
+colors.push("pink");
+
+// add another name
+names.push("Ed");
