28848f5abb8058f404af7c2e72b4346952b64b56
SchmuhlFace <eschmuhl@gmail.com>
2016-09-18 22:34:36 -0400

    Add algos file and psuedocode

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+
+
+
+//PSEUDOCODE
+
+// examine three different phrases in a collection.
+// determine each phrase's word count.
+// compare three phrases by their word count.
+// determine which phrase is the longest.
+// return only the longest phrase of the three phrases.
