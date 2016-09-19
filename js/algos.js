36137648a0f6505514df4854bf15e4f8a6db9379
SchmuhlFace <eschmuhl@gmail.com>
2016-09-18 23:29:31 -0400

    add function and test with driver code and multiple phrases that are variables

--- js/algos.js
+++ js/algos.js
@@ -1,10 +1,39 @@
 
 
 
-//PSEUDOCODE
+//PSEUDOCODE (w/o code jargon as much as possible!)
 
-// examine three different phrases in a collection.
-// determine each phrase's word count.
-// compare three phrases by their word count.
+// create a label to reference a collection of three phrases.
+// make a way to examine three different phrases in a collection (function)
+// make sure to set up a counter of 0 to compare the length of the phrase to something.
+// have the function go over each phase and count how long it is.
 // determine which phrase is the longest.
 // return only the longest phrase of the three phrases.
+
+var phrases = ["long phrase","longest phrase","longer phrase"]
+var animals = ["cheetah","lion" ,"minx", "mister tumnis"]
+var authors = ["banana yoshimoto","haruki murakami", "kawabate"]
+
+function determineLongest(array) {
+  var letterCounter = 0;
+  var wordLongest = '';
+
+  for(var i=0; i < array.length; i++){
+    if(array[i].length > letterCounter){
+      var letterCounter = array[i].length;
+      var wordLongest = array[i];
+    }
+  }
+  return wordLongest;
+}
+
+//drivercode testing
+
+// console.log(determineLongest(phrases))
+// console.log(determineLongest(animals))
+// console.log(determineLongest(authors))
+
+// longest phrase
+// mister tumnis
+// banana yoshimoto
+// => undefined
