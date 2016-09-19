f7fafa242248707653f87bc2725737acacf40ae8
SchmuhlFace <eschmuhl@gmail.com>
2016-09-19 08:55:37 -0400

    Add function that compares the keys of two objects and test

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,5 @@
 
-
+// Release *0*
 
 //PSEUDOCODE (w/o code jargon as much as possible!)
 
@@ -9,6 +9,8 @@
 // have the function go over each phase and count how long it is.
 // determine which phrase is the longest.
 // return only the longest phrase of the three phrases.
+
+//RELEASE 0
 
 var phrases = ["long phrase","longest phrase","longer phrase"]
 var animals = ["cheetah","lion" ,"minx", "mister tumnis"]
@@ -27,7 +29,31 @@ function determineLongest(array) {
   return wordLongest;
 }
 
-//drivercode testing
+// RELEASE 1
+
+// write a function that takes two objects and checks to see if the objects share at least one key-value pair
+
+// set up a variable for the whole things
+var compare = function(thing1, thing2) {
+  for (var key in thing1) {
+    if (thing2[key] == thing1[key]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//set up variables as hashes b/c they store two pieces of info
+var thing1 = {name: "Steven", age: 54};
+var thing2 = {name: "Tamir", age: 54};
+var result = compare(thing1,thing2);
+
+console.log(result);
+
+
+//DRIVERCODE TESTING
+
+// for ***Release 0***
 
 // console.log(determineLongest(phrases))
 // console.log(determineLongest(animals))
@@ -37,3 +63,15 @@ function determineLongest(array) {
 // mister tumnis
 // banana yoshimoto
 // => undefined
+
+// for ***Release 1***
+
+
+// for ***Release 2***
+
+
+
+
+
+
+
