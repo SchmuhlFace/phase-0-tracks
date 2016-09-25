f1fa9db0eb4b79c509953034bc870c1d4eea2e7c
SchmuhlFace <eschmuhl@gmail.com>
2016-09-25 10:13:32 -0400

    Add release two working with hashes and arrays and looking up documentation for built in calls

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -71,96 +71,39 @@ end
 # The fox has 5 babies.
 # => {:frog=>"2", :turtle=>"100", :fox=>"5"}
 
-## .map notes
-
-
-
-
-
-
-
-
-
-
-
-
-
-5.3 Pairing lsw0011 & SchmuhlFace
-
-##Release 0
-
-def method
-
-  puts "Why, hello there!"
-  yield("Steve", "Tiahna" , "Dali")
-end
-
-method { |name1, name2, name3| puts "great to see you, #{name1} and #{name2} and #{name3}"}
-
-
-## Release 1
-
- array = ["a", "b", "c", "d"]
- hash = { 1 => 'one' , 2 => 'two', 3 => 'three'}
-
- array.each do |letter|
- 	print "#{letter}\n"
- end
-
- print "#{hash}\n"
-
- hash.each do |digit, number|
- 	print "#{digit} means #{number}\n"
- end
-
- hash.map {|digit, number| digit = digit+1}
- print hash
-
-new_array = [1,2,4,3,5,6,7,8,9,10]
-
-for var in new_array.length
-	if 0 == new_array[var]%2 
-		new_array.del_at(var)
-	end
-end
-
-print "#{h}\n"
-	old_array.delete_if { |number| number%2 == 0 }
-
 
 ##Release 2
 
-#1
+#arrays
 
 old_array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-  old_array = old_array.select{|x| x.odd?}
+  
+   old_array = old_array.select{|x| x.odd?}
+   # => [1, 3, 5, 7, 9, 11, 13, 15]
 
-ole_hash = {
+   old_array = old_array.drop(3)
+   # => [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+   old_array = old_array.drop_while{|number| number < 5 }
+   # => [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+
+#hashes 
+animal_hash = {
   'bunny' => 'intact',
   'rabbit' => 'intact',
   'hare' => 'intact',
   'rodent' => 'intact'
 }
 
-ole_hash = ole_hash.select{|x|}
-print ole_hash
+animal_hash = animal_hash.delete_if{|animal, status| animal == "bunny"}
+# => {"rabbit"=>"intact", "hare"=>"intact", "rodent"=>"intact"}
 
-#2
+animal_hash = animal_hash.reject{|animal, status| animal == "bunny"}
+# => {"rabbit"=>"intact", "hare"=>"intact", "rodent"=>"intact"}
 
-old_array = old_array.delete_if{|x| x.odd?}
-ole_hash = ole_hash.delete_if{|animal, status| animal == "bunny"}
-
-#3
-
-old_array = old_array.drop(3)
-ole_hash = ole_hash.reject{|animal, status| animal == "bunny"}
-
-#4
-
-ole_hash = ole_hash.keep_if{|animal,status| animal == "bunny" or animal == "hare"}
-print ole_hash
-old_array = old_array.drop_while{|number| number < 5 }
-
+animal_hash = animal_hash.keep_if{|animal,status| animal == "bunny" or animal == "hare"}
+# => {"bunny"=>"intact", "hare"=>"intact"}
 
 
 
