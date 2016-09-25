76f5d15a7838699eb5dfefc6174975fff35e7178
SchmuhlFace <eschmuhl@gmail.com>
2016-09-25 17:47:02 -0400

    Add release 2 build method for Fibanocci sequence

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -7,7 +7,6 @@
 #if it is, return the index value
 #if it is not, return nil
 
-
 def search_array(a, num)
   i = 0
   while i < a.length 
@@ -25,9 +24,6 @@ p search_array(a, 23)
 a = [21, 27, 1, 5]
 p search_array(a, 1)
 => 2
-
-
-
 
 #Release 1
 
@@ -49,31 +45,20 @@ end
 
 puts fibonacci(100)
 
-#Release 2
-
-#Psuedocode
-
-#Taken a list of data and evaluate each item of the list
-#Reorganize the list based on numerical value
-#Reorder the list in ascending order
 
 
 
-def insertion_sort(array)
-  (1..array.length-1).each do |i|
-    insertion_value = array.delete_at(i)
 
-    index_inserted = i
-    index_inserted -= 1 while index_inserted > 0 && insertion_value < array[index_inserted -1]
 
-    array.insert(index_inserted, insertion_value)
-  end
 
-  array
-end
 
-ex_array = [ 1, 5, 3, 17, 4, 25, 9]
-p insertion_sort(ex_array)
+
+
+
+
+
+
+
 
 
 
