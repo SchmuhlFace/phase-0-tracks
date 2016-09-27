aca1412395a0856aeeaed6546bf3ec087bd93479
SchmuhlFace <eschmuhl@gmail.com>
2016-09-26 21:48:28 -0400

    Add genders and ethnicities and put them into an array

--- ruby/santa.redo.rb
+++ ruby/santa.redo.rb
@@ -1,7 +1,11 @@
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -17,10 +21,25 @@ end
 
 # driver code
 
-bb_santa = Santa.new
+# bb_santa = Santa.new
 
-p bb_santa.speak
-p bb_santa.eat_milk_and_cookies("snickerdoodle")
+# p bb_santa.speak
+# p bb_santa.eat_milk_and_cookies("snickerdoodle")
+
+
+santas = []
+
+example_genders = ["butterfly", "female", "bigender", "male", "female", "gender fluid", "rainbow"]
+example_ethnicities = ["black", "latina", "white", "japanese", "human", "unicorn", "wizard"]
+
+
+example_genders.length.times do |i|
+santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+
+
+
 
 # Initializing Santa instance...
 # Ho, ho, ho! Haaaappy holidays!
