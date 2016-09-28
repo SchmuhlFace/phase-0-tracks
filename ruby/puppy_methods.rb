a969ffce876a59cde0500ab98a90a8eb38215724
SchmuhlFace <eschmuhl@gmail.com>
2016-09-27 21:36:24 -0400

    I was naviagtor and evan was driver. this is our work I'm pushing to github

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,6 +1,5 @@
-# Pairing partners: 
-  # zaslam72
-  # schmuhlface
+#Pairing w/ Evanrenaud & Schmuhlface
+
 
 class Puppy
 
@@ -9,74 +8,63 @@ class Puppy
     toy
   end
 
-  def speak(x)
-    x.times do 
-      puts "Woof!"
-    end
+  def speak(times)
+    puts "Woof!" * times
   end
 
-  def roll_over
+  def roll_over()
     puts "*rolls over*"
   end
 
-  def dog_years(n)
-    dog_age = n * 7
-    p dog_age
+  def dog_years(human_years)
+    dog_years = human_years * 7
   end
 
-  def sit
-    puts "Sit!"
+  def play_dead()
+    puts "*lies down on its back*"
   end
 
-  def initialize
+  def initialize()
     puts "Initializing new puppy instance..."
   end
 
 end
 
+# Buddy = Puppy.new
+# Buddy.fetch("ball")
+# Buddy.speak(5)
+# Buddy.roll_over
+# p Buddy.dog_years(4)
+# Buddy.play_dead
 
-# Driver code
+class Food
 
-maxine = Puppy.new
-maxine.fetch("ball")
-maxine.speak(4)
-maxine.roll_over
-maxine.dog_years(3)
-maxine.sit
+  def prepare(meal)
+    puts "It's time to eat #{meal}"
+    meal
+  end
 
-
-class Fruit
+  def calories(cal)
+    puts "You have eaten #{cal} calories."
+    cal
+  end
 
   def initialize
-    puts "Initializing new fruit instance..."
-  end
-
-  def apples_in_basket(a)
-    a.times do 
-      puts "An apple a day keeps the doctor away!"
-    end
-  end
-
-  def color_of_apple(color)
-    puts "This is such a lovely #{color} apple!"
+    puts "What's cookin?"
   end
 
 end
 
-# Driver code
+class_array = []
 
-apple = Fruit.new
-apple.apples_in_basket(4)
-apple.color_of_apple("green")
-
-apples = []
-50.times {apple = Fruit.new
-  apples.push(apple)}
- 
-p apples 
-
-apples.each do |apple|
-  apple.apples_in_basket(2)
-  apple.color_of_apple("red")
+50.times do |instance|
+  instance = Food.new
+  class_array << instance
 end
+
+class_array.each do |instance|
+  instance.prepare("breakfast")
+  instance.calories(500)
+end#
+
 
