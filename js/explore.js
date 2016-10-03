df9fcd4e281520f0224cbe268466003638144ad0
SchmuhlFace <eschmuhl@gmail.com>
2016-10-03 16:54:13 -0400

    test function with a word and include the retrun and then comment it out

--- js/explore.js
+++ js/explore.js
@@ -1,19 +1,9 @@
-// pseudocode a function that takes a string as a parameter 
-// and reverses the string. reverse("hello"), for example, should 
-// return "olleh".  use phrases 
-// like "for each" instead of JavaScript-specific terms. 
-// Remember that you can add strings in JavaScript, so '' + 'a' 
-// would result in the string 'a'. Take your time. This is the 
-// most valuable skill in programming: being able to think through 
-// a problem logically.
-
-
 //PSEUDOCODE
 
 // A function is like a Ruby method... so:
 // Take in a string
 // Split that string into individual letters in an array
-// Reverse all the letters
+// Reverse all the letters in the array
 // Join them all back together into a string
 
 
@@ -58,8 +48,9 @@ function reverseWord(str) {
     }
     return newWord;
 }
-// reverseWord('hello');
-var newWord = reverseWord('hello');
+
+// reverseWord('snow');
+// => 'wons'
 
 // 2. Add driver code that prints the variable if some condition is 
 //true. The condition can be a silly one, like 1 == 1.
