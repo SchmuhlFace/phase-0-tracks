3844f3a50d0cf2e16ea59e3e7e3bee4765aa2f84
SchmuhlFace <eschmuhl@gmail.com>
2016-10-14 15:09:20 -0400

    Delete old databse and add rainbow gem and make sure it works and it does

--- databases/better_life/better_life.rb
+++ databases/better_life/better_life.rb
@@ -44,30 +44,30 @@ mindful_meditation = ["Guided", "Pranayama", "Open-eyed", "Walking", "Dancing",]
 # User Interface
 
 
-puts Rainbow("Hello and welcome to *** WELL BEING ***, your app to a more mindful life").underline.bright
-puts "Please enter your name"
+puts Rainbow("Hello and welcome to *** WELL BEING ***, your app to a more mindful life").underline.red
+puts Rainbow("Please enter your name").orange
 user_input = gets.chomp
-puts "Hello #{user_input}! We know planning your meditations can be difficult. That's why we're happy to generate them for you! If you're ready to receive your first suggested meditation instructions, please type *I'm ready*. If you're not ready, please type *I'm not ready*"
+puts Rainbow("Hello #{user_input}! We know planning your meditations can be difficult. That's why we're happy to generate them for you! If you're ready to receive your first suggested meditation instructions, please type *I'm ready*. If you're not ready, please type *I'm not ready*").yellow
   user_status = gets.chomp
    
     if user_status == "I'm not ready"
-    puts "That's okay. Come back when you're ready to be a Well Being! NAMASTE."
+    puts Rainbow("That's okay. Come back when you're ready to be a Well Being! NAMASTE.").green
 
     elsif user_status == "I'm ready"
-      puts "Wonderful! Please enter the number of meditations you'd like."
+      puts Rainbow("Wonderful! Please enter the number of meditations you'd like.").green
       med_number = gets.chomp.to_i
-      puts "Thank you! We're preparing your request of #{med_number} meditations, #{user_input}. Take a deep breath!"
-      puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+      puts Rainbow("Thank you! We're preparing your request of #{med_number} meditations, #{user_input}. Take a deep breath!").blue
+      puts Rainbow("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*").blink.blue
         med_number.times do
         create_mindful_day(db, mindful_meditation.sample, Faker::Address.city, Faker::Address.state, Faker::Hipster.word )
         end
 
             better_life = db.execute("SELECT * from better_life")
             better_life.each do |suggestion|
-              puts "Your mindful meditation of #{suggestion['mindful_meditation']} would be best practiced in #{suggestion['city']} , #{suggestion['state']} while repeating the mantra *** #{suggestion['word']} ***."
-              puts "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+              puts Rainbow("Your mindful meditation of #{suggestion['mindful_meditation']} would be best practiced in #{suggestion['city']} , #{suggestion['state']} while repeating the mantra *** #{suggestion['word']} ***.").magenta
+              puts Rainbow("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*").blink.blue
             end
-            puts "Thank you for using WELL BEING! NAMASTE"
+            puts Rainbow("Thank you for using WELL BEING! NAMASTE").red
           end
 
 
