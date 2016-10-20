a45d8e73199ed4307d17cc5389169ef6abe1319e
SchmuhlFace <eschmuhl@gmail.com>
2016-10-20 19:17:42 -0400

    fixed our campus table and everything works

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,9 +7,6 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
-campuses = SQLite3::Database.new("campuses.db")
-campuses.results_as_hash = true
-
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
@@ -33,17 +30,12 @@ post '/students' do
 end
 
 get '/students/campuses' do
-  @campuses = campuses.execute("SELECT * FROM campuses")
+  @campuses = db.execute("SELECT * FROM campuses")
   erb :campuses
 end
 
-post '/students/campus' do
-  campuses.execute("INSERT INTO campuses (campus) VALUES (?)", [params['campus']])
+post '/campuses' do
+  db.execute("INSERT INTO campuses (campus) VALUES (?)", [params['campus']])
+  redirect '/students/campuses'
 end
 
-# get '/students/search' do
-#   @search = db.execute("SELECT * FROM students WHERE name= ? AND campus= ? AND age= ?", [params['name'], params['campus'], params['age'].to_i])
-# end
-
-
-# add static resources
