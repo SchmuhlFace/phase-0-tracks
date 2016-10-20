5ea339bb025aa32b4f81d19dbc3be10fc480b2ef
SchmuhlFace <eschmuhl@gmail.com>
2016-10-20 19:01:58 -0400

    create campus table'

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,6 +7,9 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+campuses = SQLite3::Database.new("campuses.db")
+campuses.results_as_hash = true
+
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
@@ -27,6 +30,11 @@ end
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
+end
+
+get '/students/campuses' do
+  @campuses = campuses.execute("SELECT * FROM campuses")
+  erb :campuses
 end
 
 # get '/students/search' do
