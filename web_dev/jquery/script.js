13a19e6f9cd8c2cd32882cd91a156dbab1ff3190
SchmuhlFace <eschmuhl@gmail.com>
2016-10-21 12:43:15 -0400

    poem and site is working a click button now i will try hover

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,21 +1,8 @@
-$(function(){
-    $("#poem").mouseout(function(){
-        $("#poem").animate({
-            opacity: 0.0
-        });
-    });
-});
 
 $(function(){
-    $("span").click(function(){
-        $("#poem").animate({opacity: 1});
-    });
-});
-
-$(function(){
-    $("#poem").hover(function(){
+    $("#click").click(function(){
         $("#peach").animate({
-            left: '400px'
-        }, "slow");
+            height: 'toggle'
+        });
     });
 });
